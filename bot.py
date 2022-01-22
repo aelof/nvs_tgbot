a75824edb8033a3acbe91f514b7b282591178ee8
alex <bestreality@mail.ru>
2022-01-22 14:41:21 +0300

    structure optimization

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import telebot
 from config import TOKEN
 from keyboards import (general_markup, geo_markup, kush_markup,
-                       kush_house_markup, phone_markup, hello)
+                       kush_house_markup, menu_markup)
+from helpers import hello, State, category_list
 
-bot = telebot.TeleBot(
-    TOKEN,
-    parse_mode='HTML')  # You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot(TOKEN,
+                      parse_mode='HTML')  # You can set parse_mode by default. HTML or MARKDOWN
 
 
 @bot.message_handler(commands=['start', 'sendtoall'])
@@ -14,18 +14,16 @@ def send_welcome(message):
     name = message.from_user.first_name
     if message.text == '/start':
         bot.send_message(chat_id,
-                     'Здравствуйте, ' + str(name) + hello,
-                     reply_markup=general_markup)
+                         'Здравствуйте, ' + str(name) + hello,
+                         reply_markup=menu_markup)
     elif message.text == '/sendtoall':
         msg = bot.send_message(chat_id,
-                           'Отправьте мне то, что хотите разослать другим')
+                               'Отправьте мне то, что хотите разослать другим')
         bot.register_next_step_handler(msg, sendtoall)
-
 
 def sendtoall(message):
     try:
         chat_id = message.chat.id
-        text = message.text
         bot.send_message(chat_id,
                          'Ваше сообщение отправлено всем пользователям бота')
     except Exception as e:
@@ -33,135 +31,50 @@ def sendtoall(message):
                          'Что-то пошло не так, попробуйте заново',
                          reply_markup=general_markup)
 
+@bot.message_handler(func=lambda query: query.text == 'Найти объект')
+def search_obj(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id,
+                     'Хорошо 👌\nсейчас я помогу подобрать Вам объект\
+                            \nвыберете, что Вас интересует',
+                     reply_markup=general_markup)
 
-@bot.message_handler(func=lambda query: query.text == 'Инвестиции')
+
+@bot.message_handler(func=lambda query: query.text in category_list)
 def investment(message):
+    State.category = message.text
     chat_id = message.chat.id
     msg = bot.send_message(chat_id,
                            'Давайте выберем город:',
                            reply_markup=geo_markup)
     bot.register_next_step_handler(msg, choice_geo)
-
-
-@bot.message_handler(func=lambda query: query.text == 'Земельные участки')
-def investment(message):
-    chat_id = message.chat.id
-    msg = bot.send_message(chat_id,
-                           'Давайте выберем город:',
-                           reply_markup=geo_markup)
-    bot.register_next_step_handler(msg, choice_geo)
-
-
-@bot.message_handler(func=lambda query: query.text == 'Дома')
-def investment(message):
-    chat_id = message.chat.id
-    msg = bot.send_message(chat_id,
-                           'Давайте выберем город:',
-                           reply_markup=geo_markup)
-    bot.register_next_step_handler(msg, choice_geo_house)
-
-
-@bot.message_handler(func=lambda query: query.text == 'Видео обзоры')
-def investment(message):
-    chat_id = message.chat.id
-    msg = bot.send_message(chat_id,
-                           'Давайте выберем город:',
-                           reply_markup=geo_markup)
-    bot.register_next_step_handler(msg, choice_geo_video)
-
-
-def choice_geo_house(message):
-    try:
-        chat_id = message.chat.id
-        text = message.text
-        if text == 'Геленджик':
-            msg = bot.send_message(chat_id,
-                                   'Давайте выберем бюджет:',
-                                   reply_markup=kush_house_markup)
-            bot.register_next_step_handler(msg, choice_kush_house)
-        elif text == 'Анапа':
-            msg = bot.send_message(chat_id,
-                                   'Давайте выберем бюджет:',
-                                   reply_markup=kush_house_markup)
-            bot.register_next_step_handler(msg, choice_kush_house)
-        elif text == 'Лаго-Наки':
-            msg = bot.send_message(chat_id,
-                                   'Давайте выберем бюджет:',
-                                   reply_markup=kush_house_markup)
-            bot.register_next_step_handler(msg, choice_kush_house)
-    except Exception as e:
-        bot.send_message(chat_id,
-                         'Что-то пошло не так, попробуйте заново',
-                         reply_markup=general_markup)
-
-
-def choice_kush_house(message):
-    try:
-        chat_id = message.chat.id
-        text = message.text
-        if text in ['5 - 7 млн', '7 - 10 млн', '10+ млн']:
-            bot.send_message(
-                chat_id,
-                'Отправляется ссылка из ЦРМ по домам согласно городу и бюджету',
-                reply_markup=general_markup)
-    except Exception as e:
-        bot.send_message(chat_id,
-                         'Что-то пошло не так, попробуйте заново',
-                         reply_markup=general_markup)
 
 
 def choice_geo(message):
-    try:
         chat_id = message.chat.id
         text = message.text
-        if text == 'Геленджик':
-            msg = bot.send_message(chat_id,
-                                   'Давайте выберем бюджет:',
-                                   reply_markup=kush_markup)
-            bot.register_next_step_handler(msg, choice_kush)
-        elif text == 'Анапа':
-            msg = bot.send_message(chat_id,
-                                   'Давайте выберем бюджет:',
-                                   reply_markup=kush_markup)
-            bot.register_next_step_handler(msg, choice_kush)
-        elif text == 'Лаго-Наки':
-            msg = bot.send_message(chat_id,
-                                   'Давайте выберем бюджет:',
-                                   reply_markup=kush_markup)
-            bot.register_next_step_handler(msg, choice_kush)
-        else:
-            bot.send_message(chat_id,
-                             'Что-то пошло не так, попробуйте заново',
-                             reply_markup=general_markup)
-    except Exception as e:
-        bot.send_message(chat_id,
-                         'Что-то пошло не так, попробуйте заново',
-                         reply_markup=general_markup)
-
-
-def choice_geo_video(message):
-    try:
-        chat_id = message.chat.id
-        text = message.text
+        State.geo = text
         if text in ['Геленджик', 'Анапа', 'Лаго-Наки']:
-            bot.send_message(chat_id,
-                             'Отправляется ссылка на плейлист ютьюб',
-                             reply_markup=general_markup)
-    except Exception as e:
-        bot.send_message(chat_id,
-                         'Что-то пошло не так, попробуйте заново',
-                         reply_markup=general_markup)
+            msg = bot.send_message(chat_id,
+                                   'Давайте выберем бюджет:',
+                                   reply_markup=kush_house_markup)
+            bot.register_next_step_handler(msg, choice_kush)
 
 
 def choice_kush(message):
     try:
         chat_id = message.chat.id
         text = message.text
-        if text in ['до 1 млн', '1 - 3 млн', '3+ млн']:
+        State.kush = text
+        if text in ['5 - 7 млн', '7 - 10 млн', '10+ млн']:
             bot.send_message(
                 chat_id,
-                'Отправляется ссылка из ЦРМ согласно городу и бюджету',
+                f'{State.category}{State.geo}{State.kush}')
+            bot.send_message(
+                chat_id,
+                'Отправляется ссылка из ЦРМ по домам согласно городу и бюджету',
                 reply_markup=general_markup)
+
     except Exception as e:
         bot.send_message(chat_id,
                          'Что-то пошло не так, попробуйте заново',
@@ -177,13 +90,3 @@ def investment(message):
 
 
 bot.infinity_polling(interval=0, timeout=20)
-
-# def choice_geo(message):
-#     try:
-#         chat_id = message.chat.id
-#         text = message.text
-#         if text in ['Геленджик', 'Анапа', 'Лаго-Наки']:
-#             msg = bot.send_message(chat_id, 'Давайте выберем бюджет:', reply_markup=kush_markup)
-#             bot.register_next_step_handler(msg, choice_kush)
-#     except Exception as e:
-#         bot.reply_to(message, 'Что-то пошло не так, попробуйте заново через команду /start')
--- keyboards.py
+++ keyboards.py
@@ -1,10 +1,12 @@
 from telebot import types
 
-hello = '''!
-Я - специально обученный бот компании Новострой!
-Могу помочь Вам <b>то-то и то-то</b>\n
-Общаться мы будем с помощью клавиатуры с кнопками. Мне так понятнее понимать запрос.\n 
-Чем могу быть полезен? Выбери нужную кнопку  '''
+
+menu_markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, input_field_placeholder='Ориентируйтесь по кнопкам ниже')
+btn1 = types.KeyboardButton('Найти объект')
+btn2 = types.KeyboardButton('Контакты')
+btn3 = types.KeyboardButton('Заказать звонок')
+btn4 = types.KeyboardButton('Помощь')
+menu_markup.add(btn1, btn2, btn3, btn4)
 
 general_markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, input_field_placeholder='Ориентируйтесь по кнопкам ниже')
 btn1 = types.KeyboardButton('Инвестиции')
@@ -19,21 +21,16 @@ btn2 = types.KeyboardButton('Анапа')
 btn3 = types.KeyboardButton('Лаго-Наки')
 geo_markup.add(btn1, btn2, btn3)
 
-kush_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+kush_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
 btn1 = types.KeyboardButton('до 1 млн')
 btn2 = types.KeyboardButton('1 - 3 млн')
 btn3 = types.KeyboardButton('3+ млн')
 kush_markup.add(btn1, btn2, btn3)
 
 
-kush_house_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+kush_house_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
 btn1 = types.KeyboardButton('5 - 7 млн')
 btn2 = types.KeyboardButton('7 - 10 млн')
 btn3 = types.KeyboardButton('10+ млн')
 kush_house_markup.add(btn1, btn2, btn3)
-
-
-phone_markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
-btn1 = types.KeyboardButton('Телефончик', request_contact=True)
-phone_markup.add(btn1)
 
