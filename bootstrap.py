2b3667b423582a3984006b8ad5e548318a96a9a1
alex <bestreality@mail.ru>
2022-01-28 13:31:58 +0300

    update create table command

--- bootstrap.py
+++ bootstrap.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect('Users.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS users  (id integer primary key, user_id integer, user_name text)''')
+c.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, user_id INTEGER UNIQUE ON CONFLICT IGNORE, firstname TEXT, username TEXT, date INTEGER, phone INTEGER UNIQUE ON CONFLICT IGNORE, response STRING)''')
 conn.commit()
 conn.close()
